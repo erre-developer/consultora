0a821f5003426a0ee09babff9d0763d78952199d
develop-erre <richardfoncea@gmail.com>
2020-07-09 01:15:28 -0400

    Se separa seccion footer y se implementa include

---
+++ partials/footer.php
@@ -0,0 +1,40 @@
+<footer class="bg-dark text-light shadow py-4 mt-4">
+        <div class="container-fluid  pb-3">
+            <div class="row align-items-center">
+                <div class="text-center col-12 col-sm-6 col-md-6">
+                    <a target="_blank" href="https://www.facebook.com/consultoriapmo">
+                        <img src="img/facebook.svg" alt="Facebook" width="40" height="40">
+                    </a>
+                    <a target="_blank" href="https://www.youtube.com/channel/UCBmxQfuE8ddZAJBGC5_97lQ">
+                        <img src="img/youtube.svg" alt="Youtube" width="40" height="40">
+                    </a>
+                    <a target="_blank" href="https://www.linkedin.com/school/consultoria-pmo">
+                        <img src="img/linkedin.svg" alt="Linkedin" width="40" height="40">
+                    </a>
+                </div>
+                <div class="text-center miestilofooter col-12 col-sm-6 col-md-6 d-flex flex-column align-items-end ">
+                    <a class="text-success" href="index.php#nosotros">Nosotros</a>
+                    <a class="text-success" href="index.php#servicio">Servicio</a>
+                    <a class="text-success" href="index.php#certifications">Certificaciones</a>
+                    <a class="text-success" href="index.php#contacto">Contacto</a>
+                    <a class="text-success" href="index.php#contacto">Ubicacion</a>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="text-center col-12 col-sm-6 col-md-6">
+                    <h6>Consultoria <span class="text-primary">PMO</span> | Copyright © 2020</h6>
+                </div>
+                <div class="text-center col-12 col-sm-6 col-md-6">
+                    <h6 class="text-success">Desing By <a class="text-success"
+                            href="https://www.erredeveloper.cl">erre-developer</a></h6>
+                </div>
+                <div class="text-center col-12 col-sm-6 col-md-6">
+                    Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a>
+                    from <a href="https://www.flaticon.es/" title="Flaticon">
+                        www.flaticon.es</a>
+                </div>
+            </div>
+        </div>
+    </footer>
